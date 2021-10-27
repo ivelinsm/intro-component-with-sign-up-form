9aeb1bf5288c35e4f603723e18a7d2348ea974a2
Ivelin Mitev <ivelins.m@abv.bg>
2021-10-27 18:36:26 +0200

    modified:   script.js

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ function checkEmail(input) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     if (!re.test(String(input.value).toLowerCase())){
-        showError(input, `Email is not valid`)
+        showError(input, `Looks like this is not an email`)
     }
 
   }
